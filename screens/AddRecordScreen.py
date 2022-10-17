e952417469e6391ebd883d7344c22a41c3cae2a3
MisaMisimo <misamisimo@gmail.com>
2022-10-16 17:27:01 -0700

    StartingPoint - BottomNavigationAdded

---
+++ screens/AddRecordScreen.py
@@ -0,0 +1,5 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+Builder.load_file('screens/AddRecordScreen.kv')
+class AddRecordScreen(Screen):
+   pass
---
+++ screens/ViewRecordScreen.py
@@ -0,0 +1,5 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+Builder.load_file('screens/ViewRecordScreen.kv')
+class ViewRecordScreen(Screen):
+   pass
