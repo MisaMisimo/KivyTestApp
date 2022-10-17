91c15ee1306341ca0dd7f89c1e991f3dd387a40e
MisaMisimo <misamisimo@gmail.com>
2022-10-16 20:09:24 -0700

    Setup Bottom Navigation

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-# from kivy.app import App
 from kivymd.app import MDApp
 from kivy.lang import Builder
+
 from screens.AddRecordScreen import AddRecordScreen
 from screens.ViewRecordScreen import ViewRecordScreen
 
 class FinanceTrackerApp(MDApp):
-   def build(self):   
-    return Builder.load_file('main.kv')
+   def build(self):
+      self.theme_cls.theme_style = "Dark"
+      return Builder.load_file('main.kv')
 
 if __name__ == "__main__":
    FinanceTrackerApp().run()
