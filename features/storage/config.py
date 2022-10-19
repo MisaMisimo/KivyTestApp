0687f82ab0fa92879b029f8bd0e242c57a41f813
MisaMisimo <misamisimo@gmail.com>
2022-10-19 16:56:32 -0700

    Add ConfigTable, and Attribute and Table classes

--- features/storage/config.py
+++ features/storage/config.py
@@ -1,17 +1,54 @@
+class CONST():
+      DATABASE_PATH = 'database/finances.db'
 class Tables():
-   class Transactions():
-      def __init__(self, transaction_type, amount, description, date):
-         self.transaction_type = transaction_type
-         self.amount = amount
-         self.description = description
-         self.date = date
-         self.timestamp = datetime.now()
-
-   class Tags():
-      def __init__(self, name):
+   class Attribute():
+      def __init__(
+         self,
+         name,
+         data_type,
+         primary_key = False,
+         not_null = False,
+         unique = False,
+         check = False,
+         check_str = ""):
          self.name = name
-   
-   class TransactionTagRelationShip():
-      def __init__(self, transaction_key, tag_key):
-         self.transaction_key = transaction_key
-         self.tag_key = tag_key
+         self.data_type = data_type
+         # Constraints
+         self.primary_key = primary_key
+         self.not_null = not_null
+         self.unique = unique
+         self.check = check
+         self.check_str = check_str
+   class Table():
+      def __init__(self, name, attribute_list):
+         self.name = name
+         self.attribute_list = attribute_list
+      def add_attribute(self, attribute):
+         self.attribte_list.append(attribute)
+
+   configTables = [
+      Table(name = "transactions",
+         attribute_list = [
+            Attribute(name = "transaction_id",  data_type = "integer",                primary_key = True),
+            Attribute(name = "transaction_type",data_type = "integer"),
+            Attribute(name = "amount",          data_type = "decimal(65,2)"),
+            Attribute(name = "currency",        data_type = "integer"),
+            Attribute(name = "description",     data_type = "text"),
+            Attribute(name = "date",            data_type = "date"),
+            Attribute(name = "timestamp",       data_type = "timestamp"),
+         ]
+      ),
+      Table(name = "tags",
+         attribute_list = [
+            Attribute(name = "tag_id",          data_type = "integer", primary_key = True),
+            Attribute(name = "name",            data_type = "text", unique = True),
+         ]
+      ),
+      Table(name = "transaction_tag_relationship",
+         attribute_list = [
+            Attribute(name = "relation_key",    data_type = "integer", primary_key = True),            
+            Attribute(name = "transaction_key", data_type = "integer"),            
+            Attribute(name = "tag_key",         data_type = "integer"),
+         ]
+      )
+   ]
