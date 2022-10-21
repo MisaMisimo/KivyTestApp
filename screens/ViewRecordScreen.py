0be469d65c64a5a59129a79ef89aee044b6e285c
MisaMisimo <misamisimo@gmail.com>
2022-10-21 11:07:00 -0700

    initial skeleton for viewing list items

--- screens/ViewRecordScreen.py
+++ screens/ViewRecordScreen.py
@@ -1,6 +1,17 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.list import TwoLineAvatarIconListItem
 # TODO: Display records as a table
 Builder.load_file('screens/ViewRecordScreen.kv')
-class ViewRecordScreen(Screen):
+class ExpenseListItem(TwoLineAvatarIconListItem):
    pass
+class ViewRecordScreen(Screen):
+   def load_selection_list(self):
+      for i in range(10):
+         self.ids['selection_list'].add_widget(ExpenseListItem())
+   def on_selected(self):
+      pass
+   def on_unselected(self):
+      pass
+   def set_selection_mode(self):
+      pass
