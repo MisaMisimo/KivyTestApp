3044df90b0bf05e56e5f62c0d34d793eba73a1a4
MisaMisimo <misamisimo@gmail.com>
2022-10-20 22:34:41 -0700

    Add TODO

--- screens/ViewRecordScreen.py
+++ screens/ViewRecordScreen.py
@@ -1,5 +1,6 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+# TODO: Display records as a table
 Builder.load_file('screens/ViewRecordScreen.kv')
 class ViewRecordScreen(Screen):
    pass
