41a720b7083bdccdc30d743ac53d4264de66878c
MisaMisimo <misamisimo@gmail.com>
2022-10-21 19:10:09 -0700

    create DateUtil to conver between date formats

--- utils/utils.py
+++ utils/utils.py
@@ -1,2 +1,8 @@
+from datetime import datetime
 from kivy.utils import platform
 USING_ANDROID_PLTFRM = (platform == 'android')
+
+class DateUtils():
+   def convert_date_format(input_string, input_format, output_format):
+      date_time_obj = datetime.strptime(input_string,input_format )
+      return datetime.strftime(date_time_obj, output_format)
