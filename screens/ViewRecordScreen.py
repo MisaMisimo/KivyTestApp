bada50d10f39530ce3a38d9a737eb9f5e7371f92
MisaMisimo <misamisimo@gmail.com>
2022-10-25 19:31:00 -0700

    Display tags in View Record screen

--- screens/ViewRecordScreen.py
+++ screens/ViewRecordScreen.py
@@ -24,26 +24,53 @@ class ViewRecordRecycleView(RecycleView):
    def load_last_10_items(self):
       table_headers = self.interfaceStorage.get_table_headers("transactions")
       last_10_items = self.interfaceStorage.get_all_from_table_order_by("transactions","id","DESC")
+      tags = self.interfaceStorage.get_all_from_table("tags")
       for item in last_10_items:
+         # Get tags related to this transaction
+         related_tag_keys = self.interfaceStorage.get_from_table_where_equals("tag_key", "transaction_tag_relationship", "transaction_key", str(item[0]) )
+         transaction_tags = []
+         for related_tag in  related_tag_keys:
+            for tag in tags:
+               if related_tag[0] == tag[0]:
+                  transaction_tags.append(tag[1])
+                  break
+         first_string = "{:.2f}".format(item[table_headers.index('amount')]) + " " + \
+                  item[table_headers.index('currency')] + "   ||   " + \
+                  item[table_headers.index('description')]
+         second_string = DateUtils.convert_date_format(str(item[table_headers.index('date')]),"%Y-%m-%d","%B %d, %Y") + \
+                  "      "
+         for tag in transaction_tags:
+            second_string += " |" + tag + "| "
          self.RecycleViewData.append(
             {
-               "text" : "{:.2f}".format(item[table_headers.index('amount')]) + " " + \
-                  item[table_headers.index('currency')] + "   ||   " + \
-                  item[table_headers.index('description')],
-               "secondary_text": DateUtils.convert_date_format(str(item[table_headers.index('date')]),"%Y-%m-%d","%B %d, %Y")
+               "text" : first_string,
+               "secondary_text": second_string
             }
          )
    def update_data_with_last_item(self):
       table_headers = self.interfaceStorage.get_table_headers("transactions")
       last_row = self.interfaceStorage.get_last_item_from_table("transactions")
+      tags = self.interfaceStorage.get_all_from_table("tags")
+      related_tag_keys = self.interfaceStorage.get_from_table_where_equals("tag_key", "transaction_tag_relationship", "transaction_key", str(last_row[0]) )
+      transaction_tags = []
+      for related_tag in  related_tag_keys:
+         for tag in tags:
+            if related_tag[0] == tag[0]:
+               transaction_tags.append(tag[1])
+               break
+      first_string = "{:.2f}".format(last_row[table_headers.index('amount')]) + " " + \
+                  last_row[table_headers.index('currency')] + "   ||   " + \
+                  last_row[table_headers.index('description')]
+      second_string = DateUtils.convert_date_format(str(last_row[table_headers.index('date')]),"%Y-%m-%d","%B %d, %Y") + \
+                  "      "
+      for tag in transaction_tags:
+         second_string += tag + " "
       self.RecycleViewData.insert(
          0,
          {
-            "text" : str(last_row[table_headers.index('amount')]) + " " + \
-                  last_row[table_headers.index('currency')] + "   ||   " + \
-                  last_row[table_headers.index('description')],
-               "secondary_text": DateUtils.convert_date_format(str(last_row[table_headers.index('date')]),"%Y-%m-%d","%B %d, %Y")
-         }
+            "text" :first_string,
+            "secondary_text": second_string,
+         },
       )
       self.data = self.RecycleViewData
 
