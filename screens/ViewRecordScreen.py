802b4400241ee7ff14709313f925d3fd3fd99c5a
MisaMisimo <misamisimo@gmail.com>
2022-10-28 18:36:56 -0700

    Update screen even if there are no items

--- screens/ViewRecordScreen.py
+++ screens/ViewRecordScreen.py
@@ -73,7 +73,7 @@ class ViewRecordRecycleView(RecycleView):
                "transaction_info": transaction_info
             }
          )
-         self.data = self.RecycleViewData
+      self.data = self.RecycleViewData
 
 ################################################################################
 ################################################################################
