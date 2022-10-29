ca501051a1ac3e363ca6642c754643dbeec19770
MisaMisimo <misamisimo@gmail.com>
2022-10-29 14:52:10 -0700

    Add static piegraph as filler graph screen

--- screens/ViewGraphsScreen.py
+++ screens/ViewGraphsScreen.py
@@ -1,5 +1,23 @@
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from kivymd_extensions.akivymd.uix.charts import AKPieChart
+from kivy.metrics import dp
 Builder.load_file('screens/ViewGraphsScreen.kv')
 class ViewGraphsScreen(Screen):
-   pass
+   def __init__(self, **kw):
+      super(ViewGraphsScreen, self).__init__(**kw)
+   def draw_chart(self):
+      self.items = [{
+         "Electronics": 40,
+         "Laptops": 20,
+         "Shoes": 20,
+         "iPhones": 20
+      }]
+      self.piechart = AKPieChart(
+         items=self.items, pos_hint={'center_x': 0.5, 'center_y': .4},
+         size_hint=[None, None],
+         size=(dp(200), dp(200))
+      )
+      self.ids['graph_main_layout'].add_widget(self.piechart)
+   def print_button_text(self):
+      print("Hello")
