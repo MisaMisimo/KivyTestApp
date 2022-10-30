d281899447dfd6f4d8f1afa91d8c945382003b7d
MisaMisimo <misamisimo@gmail.com>
2022-10-29 22:19:10 -0700

    Pie chart will now display items from db

--- screens/ViewGraphsScreen.py
+++ screens/ViewGraphsScreen.py
@@ -2,19 +2,17 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivymd_extensions.akivymd.uix.charts import AKPieChart
 from kivy.metrics import dp
+from features.searchfilter.searchfilter import SearchFilter
 Builder.load_file('screens/ViewGraphsScreen.kv')
 class ViewGraphsScreen(Screen):
+   searchFilter = None
    def __init__(self, **kw):
       super(ViewGraphsScreen, self).__init__(**kw)
+      self.searchFilter = SearchFilter()
    def draw_chart(self):
-      self.items = [{
-         "Electronics": 40,
-         "Laptops": 20,
-         "Shoes": 20,
-         "iPhones": 20
-      }]
+      self.pie_items = [self.searchFilter.load_piechart_items()]
       self.piechart = AKPieChart(
-         items=self.items, 
+         items=self.pie_items, 
          size_hint=[0.9,0.9],
       )
       self.ids['pchart'].add_widget(self.piechart)
